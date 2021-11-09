5bc0370a6200b599a661d2beb6df0899b167d4e7
daze_77 <daze77@gmail.com>
2021-11-08 21:07:07 -0500

    updated the ticker to remove console errors

--- script2.js
+++ script2.js
@@ -296,6 +296,35 @@ function renderChart(data, months){
 }
 
 
+let tickerTape =                    {
+  "symbols": [
+  {
+      "proName": "FOREXCOM:SPXUSD",
+      "title": "S&P 500"
+  },
+  {
+      "proName": "FOREXCOM:NSXUSD",
+      "title": "Nasdaq 100"
+  },
+  {
+      "proName": "FX_IDC:EURUSD",
+      "title": "EUR/USD"
+  },
+  {
+      "proName": "BITSTAMP:BTCUSD",
+      "title": "BTC/USD"
+  },
+  {
+      "proName": "BITSTAMP:ETHUSD",
+      "title": "ETH/USD"
+  }
+  ],
+  "colorTheme": "light",
+  "isTransparent": false,
+  "showSymbolLogo": true,
+  "locale": "en"
+}
+
 
 LS()
 getNews()

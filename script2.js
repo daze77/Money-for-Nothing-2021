2c92a030c5653bb25979aadf697961d1a9c5b5e0
daze_77 <daze77@gmail.com>
2021-11-08 19:46:56 -0500

    cleaned up a bunch of debugging code and cleaned up some of the API calls to make them a little faster

--- script2.js
+++ script2.js
@@ -40,33 +40,20 @@ function LS() {
 }
 
 function stockSearch(){
-
   companySearch = document.querySelector('#stockSearch').value.toUpperCase()
-  // console.log(companySearch)
-
-
-  if((companySearch.length > 2) && (stockSymbolSearch.find(e => (e===`${companySearch}`)))){
-    // console.log(`we fount it `)
-    // console.log(companySearch.length)
-    // console.log(Boolean(stockSymbolSearch.find(e => (e!==`${companySearch}`))))
-    stockDetailAPI(companySearch)
-
-  }else if (stockSymbolSearchResults.find(e=>e.displaySymbol===`${companySearch}`)) {
-    // console.log(companySearch)
-    stockDetailAPI(companySearch)
-
-  }  else {console.log(`keep searching`)
-  
-  stockSymbolSearchAPI(companySearch)}
-
+  if((companySearch.length > 2) && (stockSymbolSearch.find(e => (e === companySearch)))){
+      stockDetailAPI(companySearch)
+  }else if (stockSymbolSearchResults.find(e => e.displaySymbol === companySearch)) {
+      stockDetailAPI(companySearch)
+  }else if(companySearch.length > 2){
+      console.log(`keep searching`)
+      stockSymbolSearchAPI(companySearch)}
 }
 
 // API to search stock symbols available - search while you type
 async function stockSymbolSearchAPI(autoQuery){
-  // console.log(autoQuery)
-
-  let stockSymbols = await fetch ('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=c2m4iqqad3idnodd7tdg').then(r=>r.json())
-  stockSymbolSearchResults = stockSymbols.filter(e => (e.description.includes(`${autoQuery}`) || e.displaySymbol.includes(`${autoQuery}`)) && (e.type === "Common Stock") 
+  let stockSymbols = await fetch ('https://finnhub.io/api/v1/stock/symbol?exchange=US&securityType=Common Stock&token=c2m4iqqad3idnodd7tdg').then(r=>r.json())
+  stockSymbolSearchResults = stockSymbols.filter(e => (e.description.includes(autoQuery) || e.displaySymbol.includes(autoQuery)) 
   && (e.mic === "XNYS"
   || e.mic === "XASE"
   || e.mic === "BATS"
@@ -79,31 +66,25 @@ async function stockSymbolSearchAPI(autoQuery){
   )
   )
   // console.log(stockSymbolSearchResults)
-
-  console.log(stockSymbols)
+  // console.log(stockSymbols)
 
   document.querySelector('#datalistOptions').innerHTML = ""
   stockSymbolSearch = []
 
-  stockSymbolSearchResults.slice(0,11).forEach(stock =>  {
+  stockSymbolSearchResults.forEach(stock =>  {
     document.querySelector('#datalistOptions').innerHTML +=
     ` <option value=${stock["displaySymbol"]} > ${stock["description"]}</option>`
     stockSymbolSearch.push({symbol: `${stock["displaySymbol"]}`, name: `${stock["description"]}`, mic: `${stock["mic"]}`})
+
   })
 
-  // console.log(`symbol array values`, stockSymbolSearch)  
 
 }
 
 // API to pull details about stock - various APIs used as this is a free tier service
 async function stockDetailAPI(symbolSelected){
-
-  // console.log(`this is the passed symbol`, symbolSelected)
-
     let todaysDate = Math.floor(Date.now()/1000)
     let aYearAgo = Math.floor((new Date().setDate(new Date().getDate()-365))/1000)
-    // console.log(`todays date is ${todaysDate} as a number and in human text ${new Date(todaysDate*1000)}`)
-    // console.log(`365 days ago was ${aYearAgo} as a number and in human txt ${new Date(aYearAgo*1000)}`)
 
 
     compDetails  = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbolSelected}&token=c2m4iqqad3idnodd7tdg`).then(r => r.json())
@@ -120,18 +101,13 @@ async function stockDetailAPI(symbolSelected){
     let timeStampYear = timeStampHumanDate.getFullYear()
 
 
-    console.log(`this is alpha corpQuote`, corpQuote)
-    console.log(`this is alpha CompDetails`, compDetails)
-    console.log(`this is alpha basicFinancials`, basicFinancials)
-    // console.log(`this is alpha candleInfo`, candleInfo)
-
     // fill the company card with details
     document.querySelector('#sharePrice').innerHTML = `Share Price (USD):  <span>$ ${corpQuote.c}</span>`
     document.querySelector('#dateNow').innerHTML = `<small>As of:  ${timeStampDay} ${timeStampMonth} ${timeStampDate}, ${timeStampYear}</small>`
 
 
     document.querySelector('#yearHigh').innerHTML = `52 Week High:  <span>$ ${(basicFinancials.metric["52WeekHigh"]).toFixed(2)} </span>`
-    document.querySelector('#yearLow').innerHTML = `52 Week Low:   <span>$ ${(basicFinancials.metric["52WeekLow"]).toFixed(2)} </span>`
+    document.querySelector('#yearLow').innerHTML = `52 Week Low:  <span>$ ${(basicFinancials.metric["52WeekLow"]).toFixed(2)} </span>`
     document.querySelector('#allEarnings').innerHTML = `Quarter Earnings Growth:  <span id="cardQRevenue" >${(basicFinancials.metric["revenueGrowthQuarterlyYoy"]).toFixed(2)}%</span>`
     document.querySelector('#allRevenue').innerHTML = `Quarter Rev Growth:  <span id="cardQEarnings" >${(basicFinancials.metric["epsGrowthQuarterlyYoy"]).toFixed(2)}%</span>`
 
@@ -282,7 +258,6 @@ function watchlist() {
 
 // when the watchlist button is pushed, pass the company name via the search function trigger
 function wlBtnSearch(tick) {
-  // console.log(tick)
   stockDetailAPI(tick)
   checkLS(tick)
 }

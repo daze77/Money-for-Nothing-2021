6ea592f8499152a8ab3ff98f4ad9f330c6292e11
daze_77 <daze77@gmail.com>
2022-04-13 13:54:10 -0400

    a little more code cleanup

--- script2.js
+++ script2.js
@@ -6,101 +6,95 @@ const months = ["January","February","March","April","May","June","July", "Augus
 
 // stock search API varibles
 let fullStockList
-
-pullStockList()
+let filterResults = []
 
 // Infor for Local Storage and Watch List
 var lswl
 var newCompany
 
-
 // API variables
-
 let compDetails = []
-let basicFinancials = []
-let candleInfo = []
 
 // check local storage for entry
 function LS() {
-    lswl = localStorage.lswl ? JSON.parse(localStorage.lswl) : []
-  
-    // if local storage has an entry populate watchlist
-    watchlist()
-  }
+  lswl = localStorage.lswl ? JSON.parse(localStorage.lswl) : []
+
+  // if local storage has an entry populate watchlist
+  watchlist()
+}
 
 
 async function pullStockList(){
     
-      fullStockList = await fetch(`https://finnhub.io/api/v1/stock/symbol?exchange=US&securityType=Common Stock&token=${token}`).then(r=>r.json())
+  fullStockList = await fetch(`https://finnhub.io/api/v1/stock/symbol?exchange=US&securityType=Common Stock&token=${token}`).then(r=>r.json())
 
-      fullStockList = fullStockList.filter(e=> e.mic === "XNYS"
-      || e.mic === "XASE"
-      || e.mic === "BATS"
-      || e.mic === "ARCX"
-      || e.mic === "XNMS"
-      || e.mic === "XNCM"
-      || e.mic === "XNGS"
-      || e.mic === "IEXG"
-      || e.mic === "XNAS"
-      )
+  fullStockList = fullStockList.filter(e=> e.mic === "XNYS"
+  || e.mic === "XASE"
+  || e.mic === "BATS"
+  || e.mic === "ARCX"
+  || e.mic === "XNMS"
+  || e.mic === "XNCM"
+  || e.mic === "XNGS"
+  || e.mic === "IEXG"
+  || e.mic === "XNAS"
+  )
 
 }
 
-let filterResults = []
 
 
 function stockSearch(){
-    let companySearch = document.querySelector('#stockSearch').value.toUpperCase()
+  const companySearch = document.querySelector('#stockSearch').value.toUpperCase()
 
-    filterResults = fullStockList.filter(list => list.description.includes(companySearch) || list.displaySymbol.includes(companySearch))
-    
-    filterResultsOptions(filterResults)
+  filterResults = fullStockList.filter(list => list.description.includes(companySearch) || list.displaySymbol.includes(companySearch))
+  
+  filterResultsOptions(filterResults)
 
-    if(companySearch.includes('-')){
-      companySearch = companySearch.split('-')
+  if(companySearch.includes('-')){
+    companySearch = companySearch.split('-')
 
-      const results = fullStockList.filter(item => item.displaySymbol === companySearch[0] && item.description.includes(companySearch[1]))
+    const results = fullStockList.filter(item => item.displaySymbol === companySearch[0] && item.description.includes(companySearch[1]))
 
-      filterResultsOptions(results)
-    }
+    filterResultsOptions(results)
+  }
 }
 
 function filterResultsOptions(res){
 
-    if(res.length === 1){
-        document.querySelector('#datalistOptions1').innerHTML = ""
-        
-        res.forEach(stock => document.querySelector('#datalistOptions1').innerHTML += `<option value=${stock.displaySymbol}-${stock.description} > ${stock.description}</option>`
-        )
-        stockDetailAPI(res[0].displaySymbol)
-    }else if(res.length <=30){
-        document.querySelector('#datalistOptions1').innerHTML = ""
-        
-        res.forEach(stock => document.querySelector('#datalistOptions1').innerHTML += `<option value=${stock.displaySymbol}-${stock.description} > ${stock.description}</option>`
-        )
-    }
+  if(res.length === 1){
+      document.querySelector('#datalistOptions1').innerHTML = ""
+      
+      res.forEach(stock => document.querySelector('#datalistOptions1').innerHTML += `<option value=${stock.displaySymbol}-${stock.description} > ${stock.description}</option>`
+      )
+      stockDetailAPI(res[0].displaySymbol)
+  }else if(res.length <=30){
+      document.querySelector('#datalistOptions1').innerHTML = ""
+      
+      res.forEach(stock => document.querySelector('#datalistOptions1').innerHTML += `<option value=${stock.displaySymbol}-${stock.description} > ${stock.description}</option>`
+      )
+  }
 }
 
 
 
 async function stockDetailAPI(symbolSelected){
 
-    let todaysDate = Math.floor(Date.now()/1000)
-    let aYearAgo = Math.floor((new Date().setDate(new Date().getDate()-365))/1000)
+    const todaysDate = Math.floor(Date.now()/1000)
+    const aYearAgo = Math.floor((new Date().setDate(new Date().getDate()-365))/1000)
 
     compDetails  = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbolSelected}&token=${token}`).then(r => r.json())
  
-    let corpQuote = await fetch(`https://finnhub.io/api/v1/quote?symbol=${symbolSelected}&token=${token}`).then(r => r.json())
+    const corpQuote = await fetch(`https://finnhub.io/api/v1/quote?symbol=${symbolSelected}&token=${token}`).then(r => r.json())
     
-    basicFinancials = await fetch(`https://finnhub.io/api/v1/stock/metric?symbol=${symbolSelected}&metric=all&token=${token}`).then(r => r.json())
+    const basicFinancials = await fetch(`https://finnhub.io/api/v1/stock/metric?symbol=${symbolSelected}&metric=all&token=${token}`).then(r => r.json())
     
-    candleInfo = await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=${symbolSelected}&resolution=M&from=${aYearAgo}&to=${todaysDate}&token=${token}`).then(r=>r.json())
+    const candleInfo = await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=${symbolSelected}&resolution=M&from=${aYearAgo}&to=${todaysDate}&token=${token}`).then(r=>r.json())
 
-    let timeStampHumanDate = new Date(corpQuote.t*1000)
-    let timeStampMonth = months[timeStampHumanDate.getMonth()]
-    let timeStampDay = days[timeStampHumanDate.getDay()]
-    let timeStampDate = timeStampHumanDate.getDate()
-    let timeStampYear = timeStampHumanDate.getFullYear()
+    const timeStampHumanDate = new Date(corpQuote.t*1000)
+    const timeStampMonth = months[timeStampHumanDate.getMonth()]
+    const timeStampDay = days[timeStampHumanDate.getDay()]
+    const timeStampDate = timeStampHumanDate.getDate()
+    const timeStampYear = timeStampHumanDate.getFullYear()
 
     // fill the company card with details
     document.querySelector('#sharePrice').innerHTML = `Share Price (USD):  <span>$ ${corpQuote.c}</span>`
@@ -117,17 +111,7 @@ async function stockDetailAPI(symbolSelected){
     document.querySelector('#cardType').innerHTML = `Sector:  ${compDetails["finnhubIndustry"]}`
 
 
-
-
     document.querySelector('.wlbtn').style.display ="block"
-
-
-
-
-
-
-
-
 
     // flag color of quarterlies - negative value is red
     if (basicFinancials.metric["revenueGrowthQuarterlyYoy"] < 0) { document.querySelector("#cardQRevenue").style.color = "red" }
@@ -135,7 +119,7 @@ async function stockDetailAPI(symbolSelected){
 
 
     // candleInfo Timestamps to Months Conversion - for chart rendering
-    let candleInfoMonths = []
+    const candleInfoMonths = []
 
     candleInfo.t.forEach(time => {
       candleInfoDates = months[(new Date(time*1000)).getMonth()]
@@ -144,34 +128,30 @@ async function stockDetailAPI(symbolSelected){
     })
     checkLS(symbolSelected)
     renderChart(candleInfo.c, candleInfoMonths)
-
-
-
-
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 //START NEWS API
 // get news API
 async function getNews() {
-    news = await fetch('https://api.nytimes.com/svc/topstories/v2/business.json?api-key=IlIdSVUvpiF5PABbTeerA3kRncTqyqAo').then(r => r.json())
-    news = news.results.filter(item => item.multimedia !== null)
-  
-    for (i = 0; i < 5; i++) {
-  
-      title = news[i].title
-      url = news[i].url
-      hotTitle = title.link(`${url}`)
-     
-      image = news[i].multimedia[0].url
-      caption = news[i].multimedia[0].caption
+  news = await fetch('https://api.nytimes.com/svc/topstories/v2/business.json?api-key=IlIdSVUvpiF5PABbTeerA3kRncTqyqAo').then(r => r.json())
+  news = news.results.filter(item => item.multimedia !== null)
+
+  for (i = 0; i < 5; i++) {
+
+    title = news[i].title
+    url = news[i].url
+    hotTitle = title.link(`${url}`)
+    
+    image = news[i].multimedia[0].url
+    caption = news[i].multimedia[0].caption
 
 
-      document.querySelector(`#newsstory${i}`).innerHTML = hotTitle
-      document.querySelector(`#nwsImg${i}`).src = image
-      document.querySelector(`#caption${i}`).innerHTML = caption 
+    document.querySelector(`#newsstory${i}`).innerHTML = hotTitle
+    document.querySelector(`#nwsImg${i}`).src = image
+    document.querySelector(`#caption${i}`).innerHTML = caption 
 
-    }
+  }
 }
   
 
@@ -181,18 +161,16 @@ async function getNews() {
 
 // Scan local storage
 function checkLS(symbol) {
-  if(e=> e.ticker === symbol){
-  }else {
-  }
-    lswl.find(e => (e.ticker===symbol)) ? 
-    (document.querySelector('.wlbtn').classList.replace("btn-success", "btn-danger"),
-    document.querySelector('.wlbtn').innerHTML = "- from Watchlist") 
-    :  (document.querySelector('.wlbtn').classList.replace("btn-danger", "btn-success"),
-    document.querySelector('.wlbtn').innerHTML = "+ to Watchlist")
 
-    document.querySelector('.wlbtn').setAttribute('data-stockSymbol', symbol)
+  lswl.find(e => (e.ticker===symbol)) ? 
+  (document.querySelector('.wlbtn').classList.replace("btn-success", "btn-danger"),
+  document.querySelector('.wlbtn').innerHTML = "- from Watchlist") 
+  :  (document.querySelector('.wlbtn').classList.replace("btn-danger", "btn-success"),
+  document.querySelector('.wlbtn').innerHTML = "+ to Watchlist")
+
+  document.querySelector('.wlbtn').setAttribute('data-stockSymbol', symbol)
     
-  }
+}
 
 
 // Watchlist button trigger (add or remove from list)
@@ -239,8 +217,7 @@ function watchListBtn() {
             <button type="button" class="btn btn-outline-light" onClick="stockDetailAPI('${tick}')">
               <span id="stkName">${nam}</span> - <span id="stkSymb">${tick}</span>
             </button>
-          </li>
-        `  
+          </li>`  
     }
   }
   
@@ -308,3 +285,4 @@ function watchListBtn() {
   LS()
   getNews()
   checkLS()
+  pullStockList()
